38a878f62b072e878bb6a3192d561613d332f992
ritz1104 <ritikrajput2611@gmail.com>
2024-06-26 21:50:26 +0530

    Update script.js

--- ZARA/script.js
+++ ZARA/script.js
@@ -1,4 +1,30 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('.main'),
-//     smooth: true
-// });
+// gsap.to(".section-2 .cards",{
+//     transform:"translate(-100%)",
+//     duration:10,
+//     scrollTrigger:{
+//         trigger :".section-2",
+//         scroller:"body",
+//         start:"top 0",
+//         end:"top -100%",
+//         pin: true,
+//         scrub:2
+//  }
+// })
+
+// gsap.to(".section-1 .text",{
+//     y:40,
+//     duration:1,
+//     opacity:0,
+//     stagger:1,
+//     scrollTrigger:{
+//         scroller:"body",
+//         trigger :".section-1",  
+//         start:"top 100%"
+
+//     }
+// })
+// gsap.to(".section-1 .text h1  ",{
+//     y:40,
+//     duration:1,
+//     opacity:0,
+// })
